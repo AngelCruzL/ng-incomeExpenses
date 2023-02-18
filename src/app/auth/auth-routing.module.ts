527b3658f1060d486dea653649125691c27a5d5b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-18 09:18:10 -0600

    feat: add auth login and register pages layout

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+import {AuthComponent} from './auth.component';
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+
+const routes: Routes = [
+  {
+    path: '', component: AuthComponent, children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'registro', component: RegisterComponent},
+      {path: '**', redirectTo: 'login'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
