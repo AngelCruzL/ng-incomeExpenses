062514f98b1db71e9964bc1a7ff87c48081af123
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-18 13:48:10 -0600

    feat: add sweet alert on auth forms

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import Swal from 'sweetalert2'
+import {getTranslatedError} from '@nafuzi/firebase-auth-error-translator'
 
 import {AuthService} from "../../services/auth.service";
 
@@ -32,13 +34,26 @@ export class LoginComponent implements OnInit {
   login() {
     if (this.loginForm.invalid) return;
 
+    Swal.fire({
+      title: 'Espere por favor...',
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
     this.authService.loginUser(this.loginForm.value)
       .then(credentials => {
+        Swal.close();
         this.router.navigate(['/'])
         console.log(credentials)
       })
       .catch(error => {
-        console.error(error)
+        Swal.fire({
+          title: '¡Error!',
+          text: getTranslatedError('es', error.code),
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
       })
   }
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import Swal from 'sweetalert2'
+import {getTranslatedError} from '@nafuzi/firebase-auth-error-translator'
 
 import {AuthService} from "../../services/auth.service";
 
@@ -33,13 +35,34 @@ export class RegisterComponent implements OnInit {
   createAccount() {
     if (this.registerForm.invalid) return;
 
+     Swal.fire({
+        title: 'Espere por favor...',
+        didOpen: () => {
+          Swal.showLoading();
+        }
+      });
+
     this.authService.createUser(this.registerForm.value)
       .then(credentials => {
+        Swal.close();
         console.log(credentials)
         this.router.navigate(['/'])
       })
       .catch(error => {
-        console.error(error)
+        if(error.message.includes('(auth/weak-password)')){
+          error.message = 'La contraseña debe tener al menos 6 caracteres.'
+        }
+
+        if(error.message.includes('(auth/email-already-in-use)')){
+          error.message = 'El correo electrónico ya existe.'
+        }
+
+        Swal.fire({
+          title: '¡Error!',
+          text: error.message,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
       })
   }
 }
