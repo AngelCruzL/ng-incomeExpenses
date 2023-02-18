46ce8c4e446f0c797a34818953de0be99606c419
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-18 12:43:24 -0600

    feat: add environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'income-expenses-app-804fc',
+    appId: '1:604447632390:web:715ed91a637f2c0e322787',
+    storageBucket: 'income-expenses-app-804fc.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAgqiC9ElCwc56Y1SAJ3UGHiDPxFrP4WpQ',
+    authDomain: 'income-expenses-app-804fc.firebaseapp.com',
+    messagingSenderId: '604447632390',
+    measurementId: 'G-PKCW1R7MCY',
+  },
+  production: false
+};
