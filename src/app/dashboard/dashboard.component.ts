097e0fc16b41d8d380652f7858e24b4a674e6256
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-21 09:57:23 -0600

    feat: add income-expense state items

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {filter, Subscription} from "rxjs";
+
+import {Store} from "@ngrx/store";
+import {AppState} from "@app/app.reducer";
+import {IncomeExpenseService} from "@app/dashboard/services/income-expense.service";
+import * as incomeExpenseActions from '../dashboard/state/income-expense.actions';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  userSubscription!: Subscription;
+  incomeExpenseSubscription!: Subscription;
+
+  constructor(
+    private store: Store<AppState>,
+    private incomeExpenseService: IncomeExpenseService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.store.select('auth')
+      .pipe(filter(({user}) => user != null))
+      .subscribe(user => {
+        this.incomeExpenseSubscription = this.incomeExpenseService.initIncomeExpenseListener(user.user?.uid!)
+          .subscribe((incomeExpenses) => {
+            const filteredIE = incomeExpenses.filter(incomeExpense => incomeExpense.amount > 0)
+            this.store.dispatch(incomeExpenseActions.setItems({items: filteredIE}))
+          })
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.incomeExpenseSubscription.unsubscribe();
+    this.store.dispatch(incomeExpenseActions.unSetItems())
+  }
 
 }
--- src/app/dashboard/services/income-expense.service.ts
+++ src/app/dashboard/services/income-expense.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {addDoc, collection, Firestore} from "@angular/fire/firestore";
+import {addDoc, collection, collectionData, Firestore} from "@angular/fire/firestore";
 
 import {IncomeExpenses} from "../models/income-expenses.model";
 import {AuthService} from "@auth/services/auth.service";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,18 @@ export class IncomeExpenseService {
   constructor(private firestore: Firestore, private authService: AuthService) {
   }
 
+  initIncomeExpenseListener(userId: string) {
+    return this.getIncomeExpensesByUser(userId)
+  }
+
   createIncomeExpense(incomeExpense: IncomeExpenses) {
     this.currentUserId = this.authService.currentUser.uid;
     const incomeExpenseRef = collection(this.firestore, this.currentUserId);
     return addDoc(incomeExpenseRef, {...incomeExpense})
+  }
+
+  getIncomeExpensesByUser(userId: string) {
+    const incomeExpenseRef = collection(this.firestore, userId);
+    return collectionData(incomeExpenseRef, {idField: 'uid'}) as Observable<IncomeExpenses[]>;
   }
 }
