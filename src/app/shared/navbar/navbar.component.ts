b649070be0c139b0b2dc01bc57277daadf0f5d40
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-21 21:39:44 -0600

    feat: display current username on sidebar and navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {filter, Subscription} from "rxjs";
+import {Store} from "@ngrx/store";
+import {AppState} from "@app/app.reducer";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  currentUser: string = '';
+  userSubscription!: Subscription;
 
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.store.select('auth')
+      .pipe(filter(({user}) => user != null))
+      .subscribe(({user}) => this.currentUser = user!.name)
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,18 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
-import {AuthService} from "../../auth/services/auth.service";
+import {AuthService} from "@auth/services/auth.service";
+
+import {AppState} from "@app/app.reducer";
+import {Store} from "@ngrx/store";
+import {filter, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
+  currentUser: string = '';
+  userSubscription!: Subscription;
+
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {
+  }
+
+  ngOnInit(): void {
+    this.userSubscription = this.store.select('auth')
+      .pipe(filter(({user}) => user != null))
+      .subscribe(({user}) => this.currentUser = user!.name)
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 
   logout() {
