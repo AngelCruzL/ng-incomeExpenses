222fd1bb7398dcabd8d04118701bf924316ab14f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-24 13:17:27 -0600

    fix: add missing required assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'income-expenses-app-804fc',
+    appId: '1:604447632390:web:715ed91a637f2c0e322787',
+    storageBucket: 'income-expenses-app-804fc.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAgqiC9ElCwc56Y1SAJ3UGHiDPxFrP4WpQ',
+    authDomain: 'income-expenses-app-804fc.firebaseapp.com',
+    messagingSenderId: '604447632390',
+    measurementId: 'G-PKCW1R7MCY',
+    databaseURL:
+      'https://income-expenses-app-804fc-default-rtdb.firebaseio.com',
+  },
 };
