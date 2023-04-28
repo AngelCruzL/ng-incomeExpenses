b453319a6d3f3685513c44a85bb5d7d94ff00caf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-28 16:18:33 -0600

    Update sidebar toggle on mobile version

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {filter, Subscription} from "rxjs";
-import {Store} from "@ngrx/store";
-import {AppState} from "@app/app.reducer";
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { filter, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '@app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
@@ -11,17 +11,29 @@ import {AppState} from "@app/app.reducer";
 export class NavbarComponent implements OnInit, OnDestroy {
   currentUser: string = '';
   userSubscription!: Subscription;
+  sidebar: any;
 
-  constructor(private store: Store<AppState>) {
+  constructor(
+    private store: Store<AppState>,
+    private elementRef: ElementRef
+  ) {
   }
 
   ngOnInit(): void {
     this.userSubscription = this.store.select('auth')
-      .pipe(filter(({user}) => user != null))
-      .subscribe(({user}) => this.currentUser = user!.name)
+      .pipe(filter(({ user }) => user != null))
+      .subscribe(({ user }) => this.currentUser = user!.name);
+
+    this.sidebar = document.querySelector('nav.sidebar');
   }
 
   ngOnDestroy(): void {
     this.userSubscription.unsubscribe();
+  }
+
+  toggleSidebar() {
+    if (this.sidebar) {
+      this.sidebar.classList.toggle('sidebar-offcanvas');
+    }
   }
 }
