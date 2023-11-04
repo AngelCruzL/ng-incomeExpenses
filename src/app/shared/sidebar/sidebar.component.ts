9894421b27b537dd9ab61cd209f7f83d6cee0d1b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-11-03 23:30:58 -0600

    Add basic driverjs tour

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,32 +1,63 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { driver } from 'driver.js';
 
-import {AuthService} from "@auth/services/auth.service";
+import { AuthService } from '@auth/services/auth.service';
 
-import {AppState} from "@app/app.reducer";
-import {Store} from "@ngrx/store";
-import {filter, Subscription} from "rxjs";
+import { AppState } from '@app/app.reducer';
+import { Store } from '@ngrx/store';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit, OnDestroy {
   currentUser: string = '';
   userSubscription!: Subscription;
+  driver;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private store: Store<AppState>
+    private store: Store<AppState>,
   ) {
+    this.driver = driver({
+      showProgress: true,
+      popoverClass: 'driverjs-theme',
+      steps: [
+        {
+          element: '#dashboard-tour',
+          popover: {
+            title: 'Dashboard',
+            description: 'Aquí puedes un panorama general de como va tu dinero',
+          },
+        },
+        {
+          element: '#income-expense-tour',
+          popover: {
+            title: 'Ingresos y Gastos',
+            description: 'Aquí puedes ver el listado de tus ingresos y gastos',
+          },
+        },
+        {
+          element: '#details-tour',
+          popover: {
+            title: 'Detalles',
+            description:
+              'Aquí puedes ver el detalle de tus movimientos registrados',
+          },
+        },
+      ],
+    });
   }
 
   ngOnInit(): void {
-    this.userSubscription = this.store.select('auth')
-      .pipe(filter(({user}) => user != null))
-      .subscribe(({user}) => this.currentUser = user!.name)
+    this.userSubscription = this.store
+      .select('auth')
+      .pipe(filter(({ user }) => user != null))
+      .subscribe(({ user }) => (this.currentUser = user!.name));
   }
 
   ngOnDestroy(): void {
@@ -34,9 +65,12 @@ export class SidebarComponent implements OnInit, OnDestroy {
   }
 
   logout() {
-    this.authService.logout()
-      .then(() => {
-        this.router.navigate(['/auth/login'])
-      })
+    this.authService.logout().then(() => {
+      this.router.navigate(['/auth/login']);
+    });
+  }
+
+  takeTour() {
+    this.driver.drive();
   }
 }
